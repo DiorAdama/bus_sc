8c6bc46407b3bffde713939fa9f00b82d3b73628
Fadel Sougou <fadelsougou@gmail.com>
2025-05-27 15:19:50 +0200

    feat: app sidebar for bus progress

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import folium
 import requests
 import polyline
 from pathlib import Path
+import random
 
 OSRM_URL = "http://router.project-osrm.org/route/v1/driving/"
 SC_COORDS = (14.713214, -17.463984)
@@ -39,19 +40,29 @@ def get_osrm_route(stops: pd.DataFrame) -> list[tuple[float, float]] | None:
         return None
     
 
-def add_route_to_map(plot: folium.Map, route_points: list[tuple[float, float]], route_stops: list[tuple[float, float, str]]) -> None:
+def add_route_to_map(
+    plot: folium.Map,
+    route_name: str,
+    route_points: list[tuple[float, float]],
+    route_stops: list[tuple[float, float, str]]
+) -> None:
     folium.Marker(
         location=route_points[0],
-        # popup=f"<b>{route_stops[0][2]}</b>",
         tooltip=route_stops[0][2],
         icon=folium.Icon(color='green', icon='play', prefix='fa')
     ).add_to(plot)
     for point in route_stops[1:-1]:
         folium.Marker(
             location=point[:2],
-            icon=folium.Icon(color='blue', icon='info-sign', prefix='fa'),
-            tooltip=point[2]
+            icon=folium.Icon(color='orange', icon='info-sign', prefix='fa'),
+            tooltip=point[2],
+            opacity=0.4
         ).add_to(plot)
+    folium.Marker(
+        location=route_points[st.session_state[route_name]], # Slightly different location
+        tooltip=f"Bus {route_name.capitalize()}",
+        icon=folium.Icon(color='blue', icon='bus', prefix='fa')
+    ).add_to(plot)
     # Add the PolyLine to highlight the road itinerary using OSRM data
     folium.PolyLine(
         locations=route_points, # Use the decoded points from OSRM
@@ -66,7 +77,6 @@ def init_map() -> folium.Map:
     plot = folium.Map(location=SC_COORDS, zoom_start=12)
     folium.Marker(
         location=SC_COORDS,
-        # popup="<b>Sacré Coeur</b>",
         tooltip="Sacré Coeur",
         icon=folium.Icon(color='red', icon='stop', prefix='fa')
     ).add_to(plot)
@@ -76,19 +86,37 @@ def build_map():
     plot = init_map()
     routes_path = Path(__file__).parent / "routes"
     route_paths = list(routes_path.rglob("**/*.csv"))
+    routes = {}
     for route_path in route_paths:
         route_frame = pd.read_csv(route_path)
         osm_route = get_osrm_route(route_frame)
         if osm_route is None:
             st.error(f"Failed to fetch route for {route_path.name}.")
             continue
-        add_route_to_map(plot, osm_route, route_frame.values.tolist())
+        routes[route_path.stem] = osm_route
+        if route_path.stem not in st.session_state:
+            st.session_state[route_path.stem] = 0
+        add_route_to_map(plot, route_path.stem, osm_route, route_frame.values.tolist())
+    build_sidebar(routes)
     return plot
 
+def build_sidebar(routes: dict[str, list[tuple[float, float]]]) -> None:
+    st.sidebar.header("Position des Bus")
+    if st.sidebar.button("Rafraichir la carte"):
+        for route_name, route in routes.items():
+            trip_length = random.randint(5, 15)
+            step = len(route)//trip_length
+            st.session_state[route_name] = (st.session_state[route_name] + step) % len(route)
+        st.rerun()
+    for route_name in routes:
+        st.sidebar.progress(
+            value = st.session_state[route_name]/(len(routes[route_name]) - 1),
+            text = f"Bus {route_name.capitalize()}",
+        )
 
+st.set_page_config(layout="wide")
 if __name__ == "__main__":
     plot = build_map()
-    st.set_page_config(layout="wide")
     st.title("Bus Sacré Coeur")
     st.components.v1.html(plot._repr_html_(), height=600, scrolling=False)
     st.write(
